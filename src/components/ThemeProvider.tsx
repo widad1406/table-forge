8a366fc01593ad7a8d4dd67fa6bc587e7fe2cb1c
Anmoldeep Singh <sainianmol16@gmail.com>
2025-09-01 13:35:59 +0530

    Update: Default theme fix

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -9,7 +9,7 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="light"
       enableSystem
       disableTransitionOnChange
       {...props}
